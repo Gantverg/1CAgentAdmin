3c6672a599ec01af2a332f63199d762d7e82e22a
Gantverg <gantvergroman@gmail.com>
2016-05-26 00:16:14 +0300

    0.0.0.1

---
+++ src/sample/Controller.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void ClickMeOnAction(ActionEvent actionEvent) {
+        System.out.println("Hello, world!");
+    }
+}
